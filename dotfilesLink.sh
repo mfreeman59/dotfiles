5a2590582e62a432c1646c79beaa2f441ced7d13
Kengo,MATSUDA <k.matsuda622@gmail.com>
2021-07-15 10:21:31 +0900

    fix dotfileLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,5 @@
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
 
